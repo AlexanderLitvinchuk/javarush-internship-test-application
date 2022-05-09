27bbdd32ac00a776b635d14b0a7f1855421e67bd
AlexanderLitvinchuk <litvinchuk.alexander7@gmail.com>
2022-05-10 00:39:46 +0300

    Доработан PlayerRepository
    расширен до JpaRepository, добавлен поиск по JpaSpecificationExecutor

--- src/main/java/com/game/repository/PlayerRepository.java
+++ src/main/java/com/game/repository/PlayerRepository.java
@@ -1,7 +1,8 @@
 package com.game.repository;
 
 import com.game.entity.Player;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PlayerRepository extends CrudRepository<Player, Long> {
+public interface PlayerRepository extends JpaRepository<Player, Long>, JpaSpecificationExecutor<Player> {
 }

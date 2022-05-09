76fb7cdda1995874d393c4df878a7cfe7880f863
AlexanderLitvinchuk <litvinchuk.alexander7@gmail.com>
2022-05-09 21:07:50 +0300

    Добавлен PlayerService и реализация

---
+++ src/main/java/com/game/service/impl/PlayerServiceImpl.java
@@ -0,0 +1,27 @@
+package com.game.service.impl;
+
+import com.game.entity.Player;
+import com.game.repository.PlayerRepository;
+import com.game.service.PlayerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlayerServiceImpl implements PlayerService {
+
+    private final PlayerRepository playerRepository;
+
+    public PlayerServiceImpl(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    @Override
+    public Iterable<Player> getPlayers() {
+        return playerRepository.findAll();
+    }
+
+    @Override
+    public Integer getPlayersCount() {
+        long count = playerRepository.count();
+        return Long.bitCount(count);
+    }
+}
